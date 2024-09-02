ead5d02ea5efa34c817f7157064a88cff766661e
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-09-02 18:05:28 -0500

    Set the stage size to 80% of the screen height and width

--- src/main/java/io/github/jeangiraldoo/tagxplorer/Main.java
+++ src/main/java/io/github/jeangiraldoo/tagxplorer/Main.java
@@ -2,8 +2,11 @@ package io.github.jeangiraldoo.tagxplorer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -11,7 +14,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Screen screen = Screen.getPrimary();
+        Rectangle2D rectangle = screen.getVisualBounds();
+        double screenWidth = rectangle.getWidth() * 0.80;
+        double screenHeight = rectangle.getHeight() * 0.80;
+        Scene scene = new Scene(fxmlLoader.load(), screenWidth, screenHeight);
         stage.setTitle("TagXplorer");
         stage.setScene(scene);
         stage.show();
