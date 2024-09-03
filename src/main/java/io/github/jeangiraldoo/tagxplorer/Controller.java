8a66799075bdf797524d97f47bb4da62bcca5901
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-09-03 12:44:17 -0500

    Implement a MenuBar on top of the searchBar

--- src/main/java/io/github/jeangiraldoo/tagxplorer/Controller.java
+++ src/main/java/io/github/jeangiraldoo/tagxplorer/Controller.java
@@ -3,6 +3,7 @@ package io.github.jeangiraldoo.tagxplorer;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.VBox;
@@ -10,20 +11,26 @@ import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
-    private ToolBar mainTool;
+    private ToolBar searchToolBar;
     @FXML
     private TextField searchBar;
     @FXML
     private Button searchButton;
     @FXML
     private VBox top;
+    @FXML
+    private MenuBar menuBar;
+
 
 
     @FXML
     public void setNodeSize(double width, double height){
-        mainTool.setPrefWidth(width);
-        searchBar.setPadding(new Insets(height * 0.008));
-        top.setPrefHeight(height * 0.10);
+        searchToolBar.setPrefWidth(width);
+        double topHeight = height * 0.08;
+        top.setPrefHeight(topHeight);
+        menuBar.setPrefHeight(topHeight * 0.3);
+        searchToolBar.setPrefHeight(topHeight * 0.7);
+
 
 
 
